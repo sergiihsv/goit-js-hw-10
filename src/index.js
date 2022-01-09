2754e80bc77b436e20af47519e0dd98e9d63258c
sergiihsv <hsv-serg@ukr.net>
2022-01-09 23:52:25 +0200

    add export

--- src/index.js
+++ src/index.js
@@ -3,3 +3,7 @@ import './css/styles.css';
 import Notiflix from 'notiflix';
 
 const DEBOUNCE_DELAY = 300;
+
+export const countriesMarkup = function (markup) {
+  document.querySelector('.country-list').innerHTML = markup;
+};
