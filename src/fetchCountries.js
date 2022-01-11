5c3630f4e1f4bd4b6f69d548744a0348ee0b1a9c
sergiihsv <hsv-serg@ukr.net>
2022-01-11 15:55:44 +0200

    10-hw start

---
+++ src/fetchCountries.js
@@ -0,0 +1,11 @@
+import Notiflix from 'notiflix';
+
+/* export const fetchCountries = function (name) {
+  fetch(
+    'https://restcountries.com/v3.1/name/${name}?fields=name.official,capital,population,flags.svg,languages',
+  )
+    .then(responce => responce.json())
+    .then(console.log);
+};
+
+fetchCountries(usa); */
--- src/index.js
+++ src/index.js
@@ -1,9 +1,24 @@
 import './css/styles.css';
+/* import { fetchCountries, countriesMarkup, countryMarkup } from './js/fetchCountries'; */
+import debounce from 'lodash.debounce';
 
 import Notiflix from 'notiflix';
 
 const DEBOUNCE_DELAY = 300;
 
-export const countriesMarkup = function (markup) {
+/* export const countriesMarkup = function (markup) {
   document.querySelector('.country-list').innerHTML = markup;
 };
+
+let name = '';
+ */
+
+function fetchCountries(name) {
+  fetch(
+    'https://restcountries.com/v3.1/name/${name}?fields=name,capital,population,flags.svg,languages',
+  )
+    .then(responce => responce.json())
+    .then(console.log);
+}
+
+fetchCountries(usa);
